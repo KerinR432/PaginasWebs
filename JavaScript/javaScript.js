ecb9f69cf668195cc88b65a9154f657d8c53abb5
Ruvik <Kerinaguilera@gmail.com>
2025-01-31 14:20:55 +0100

    Primer commit - agregar archivos

--- JavaScript/javaScript.js
+++ JavaScript/javaScript.js
@@ -1,2 +1,36 @@
-alert("Hola Mundo");
-x=x+2;
+// alert("Hola Mundo");
+//esto es un comentario
+/**
+ * comentario de varias lineas
+ */
+//variables
+/**var j=2; //puede cambiar
+let x=5; // da error cuando quieres intercambiar
+const f=1;// mantiene constante
+y = 4.5;
+econtrado=false;
+texto = "Hola Mundo";
+alert(texto);
+f=12;
+x ="Madrid"
+alert(x);
+*/
+/**x=34.63/12.31
+alert(x.toFixed(3));
+texto = "Hola"+" "+"Mundo"
+alert(texto)
+=== obliga a comparar no solo el contenido sino tambien el tipo de datos
+document.write("<h3>Hola Mundo</h3>"); //esto escribe sobre el navegador
+document.write("Esto es </br>  un <b>parrafo</b>")
+encontrado = false;
+if (encontrado) {
+    document.write("Lo he encontrado!");
+}
+else {
+    document.write("No lo he econtrado!");
+}
+*/
+for (i = 0; i < 10; i++) {
+    document.write(i);
+    document.write("<br/>")
+}
