1caf98943a711a4fcd145e7f3f37932aae0443bc
Ruvik <Kerinaguilera@gmail.com>
2025-02-11 10:27:05 +0100

    Ahora a parte de arrays y String, se introduce algunos ejercicios de JavaScripts sencillos se llego al 7

---
+++ Ejercicios_JavaScripts/js/ejerciosJs.js
@@ -0,0 +1,47 @@
+//Ejercicios 1 de boletin 1
+for (i = 0; i <= 10; i++) {
+    document.write(i + " ");
+}
+//Ejercicios 2 de boletin 1
+document.write("<hr/>");
+for (i = 0; i <= 50; i++) {
+    if (i % 2 == 0) {
+        document.write(i + " ");
+    }
+}
+//Ejercicios 3 de boletin 1
+/*document.write("<hr/>");
+var numero = prompt("Dame un numero");
+var cont = 5;
+for (let i = 1; i <= 5; i++) {
+    var multiplo = numero * i;
+    resultado = multiplo + " ";
+    document.write(resultado);
+} */   
+//Ejercicios 4 de boletin 1
+document.write("<hr/>");
+for(i=0;i<=10000;i++){
+    if(i%7==0){
+        document.write(i+" ");
+    }
+}
+//Ejercicios 5 de boletin 1
+/*document.write("<hr/>");
+var numero1 = prompt("Dame un numero");
+if(numero1%2==0){
+    document.write("El numero es par "+numero1.toString());
+}
+else{
+    document.write("El numero es impar "+numero1.toString());
+}*/
+//Ejercicios 6 de boletin 1
+document.write("<hr/>");
+var numero2 = prompt("Dame un numero");
+if(numero2%3==0){
+    document.write("El numero es divisble de 3");
+}
+else{
+    document.write("El numero no es divisble de 3");
+}
+//Ejercicios 7 de boletin 1
+document.write("<hr/>");
