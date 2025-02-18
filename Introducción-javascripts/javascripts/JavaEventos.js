90f0784b1ef94b9cce9e5a1e18707f701b90d154
Ruvik <Kerinaguilera@gmail.com>
2025-02-18 09:12:36 +0100

    Manipolazción del javascript, tanto en html como en css

--- Introducción-javascripts/javascripts/JavaEventos.js
+++ Introducción-javascripts/javascripts/JavaEventos.js
@@ -1,4 +1,21 @@
-function mifuncion(){
+/**function mifuncion(){
     texto=prompt("Que titulo quieres poner?");
     titular.innerHTML = texto;
+}*/
+function mifuncion2(titular) {//funciones que podemos almacenar funciones de evento
+    parrafo = document.getElementById("parrafo2");
+    /*console.log("Debuggeando...");  --> dejar mensaje en la consola
+    debugger; // --> detiene la ejecución del código*/
+    tmp = titular.innerHTML;
+    titular.innerHTML = parrafo.innerHTML;
+    parrafo.innerHTML = tmp;
+    //esto intercambia el texto de dos objetos
+    temporal = titular.className;
+    alert(temporal);
+    if (titular.className == 'rojo') {
+        titular.className = 'azul';
+
+    } else {
+        titular.className = 'rojo';
+    }
 }
