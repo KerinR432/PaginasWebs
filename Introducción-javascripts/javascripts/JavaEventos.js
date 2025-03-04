a098bb4ff0d9f4d254b1a1614a925021770065a6
KerinR432 <kerinaguilera@gmail.com>
2025-03-04 10:20:34 +0100

    Eventos JS 2

--- Introducción-javascripts/javascripts/JavaEventos.js
+++ Introducción-javascripts/javascripts/JavaEventos.js
@@ -20,47 +20,47 @@
 //     }
 // }
 //¡CREAR EVENTOS SIN TOCAR EL HTML!
-// caja1 = document.getElementById("izquerda");
-// caja2 = document.getElementById("derecha");
-
-// caja1.addEventListener("mouseover", cambiar);
-// caja1.addEventListener("mouseout", cambiarNormal);
-// caja2.addEventListener("mouseover", cambiar);
-// caja2.addEventListener("mouseout", cambiarNormal);
-
-// function cambiar() {
-//     caja1.className = "cajaamarilla";
-//     caja2.className = "cajamarilla";
-//     caja1.innerHTML = "<h2>Box 1</h2>";
-//     caja2.innerHTML = "<h2>Box 2</h2>";
-// }
-
-// function cambiarNromal() {
-//     caja1.className = "cajanormal";
-//     caja2.className = "cajanormal";
-//     caja1.innerHTML = "<h2>caja 1</h2>";
-//     caja2.innerHTML = "<h2>caja 2</h2>";
-// }
-
-//¡SUSCRIBIR ELEMENTOS
 Caja1 = document.getElementById("izquierda");
 Caja2 = document.getElementById("derecha");
 
-Caja1.addEventListener("click", cambiar);
-Caja1.addEventListener("mouseout", cambiarNormal);
-Caja2.addEventListener("click", cambiar);
+Caja1.addEventListener("mouseover", cambiar);
+caja1.addEventListener("mouseout", cambiarNormal);
+Caja2.addEventListener("mouseover", cambiar);
 Caja2.addEventListener("mouseout", cambiarNormal);
 
 function cambiar() {
+    Caja1.className = "cajaAmarilla";
+    Caja2.className = "cajaAmarilla";
     Caja1.innerHTML = "<h2>Box 1</h2>";
     Caja2.innerHTML = "<h2>Box 2</h2>";
-    Caja1.style.backgroundColor='pink';
-    Caja2.style.backgroundColor='lightgreen';
 }
 
 function cambiarNormal() {
+    Caja1.className = "cajaNormal";
+    Caja2.className = "cajaNormal";
     Caja1.innerHTML = "<h2>caja 1</h2>";
     Caja2.innerHTML = "<h2>caja 2</h2>";
-    Caja1.style.backgroundColor='white';
-    Caja2.style.backgroundColor='white';
 }
+
+//¡SUSCRIBIR ELEMENTOS
+// Caja1 = document.getElementById("izquierda");
+// Caja2 = document.getElementById("derecha");
+
+// Caja1.addEventListener("click", cambiar);
+// Caja1.addEventListener("mouseout", cambiarNormal);
+// Caja2.addEventListener("click", cambiar);
+// Caja2.addEventListener("mouseout", cambiarNormal);
+
+// function cambiar() {
+//     Caja1.innerHTML = "<h2>Box 1</h2>";
+//     Caja2.innerHTML = "<h2>Box 2</h2>";
+//     Caja1.style.backgroundColor='pink';
+//     Caja2.style.backgroundColor='lightgreen';
+// }
+
+// function cambiarNormal() {
+//     Caja1.innerHTML = "<h2>caja 1</h2>";
+//     Caja2.innerHTML = "<h2>caja 2</h2>";
+//     Caja1.style.backgroundColor='white';
+//     Caja2.style.backgroundColor='white';
+// }
