22da747d32c7490bde7ed001d40f575522dc51e6
KerinR432 <kerinaguilera@gmail.com>
2025-03-25 09:12:37 +0100

    deja de robarme freddy

--- Introducción-javascripts/javascripts/Ciudadades.js
+++ Introducción-javascripts/javascripts/Ciudadades.js
@@ -1,7 +1,26 @@
-function eleccion(Cont){
-   var continente = document.getElementById("continente").value;  
-    if(continente == "Europa"){
-        es.disabled = true;
-    }
+function cambiarContinene(elemento){
+   continente = elemento.value;
+   if(continente == "Europa"){
+    Marrakech.disabled = true;
+    Madrid.disabled = false;
+   }
+   if(continente == "America"){
    
+   }
+   if(continente == "Asia"){
+    
+   }
+   if(continente == "Africa"){
+    Marrakech.disabled = false;
+    Ciudad_del_Cabo.disabled = false;
+    El_Cairo.disabled = false; 
+    Madrid.disabled = true;
+    Londres.disabled = true;
+    Berlin.disabled = true;
+   }
+   if(continente == "Oceania"){
+    
+   }else{
+
+   }
 }
